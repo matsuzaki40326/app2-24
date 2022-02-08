816236c82285c93f4ba0781df1592003d699f3d8
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-02-08 12:37:11 +0000

    13章 finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
    root to: 'homes#top'
    devise_for :users
    
-   resources :post_images, only: [:new, :index, :show, :create]
+   resources :post_images, only: [:new, :index, :show, :create, :destroy]
   
   get 'homes/about' , as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
